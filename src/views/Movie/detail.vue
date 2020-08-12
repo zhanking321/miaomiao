249cd15e27f22c865f0f28dc9e6389409cba6073
zhanking321 <952523586@qq.com>
2020-08-12 16:26:41 +0800

    bug clear

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -6,7 +6,7 @@
         <loading v-if="isLoading"></loading>
 		<div v-else id="content" class="contentDetail">
 			<div class="detail_list">
-				<div class="detail_list_bg"></div>
+				<div class="detail_list_bg" :style="{'background-image':'url('+detailMovie.poster+')'}"></div>
 				<div class="detail_list_filter"></div>
 				<div class="detail_list_content">
 					<div class="detail_list_img">
@@ -74,7 +74,7 @@ export default {
         }
     },
     mounted() {
-        // window.onscroll = this.handleScroll
+        window.onscroll = this.handleScroll
         this.axios({
             url: `https://m.maizuo.com/gateway?filmId=${this.id}&k=5319310`,
             headers: {
@@ -102,7 +102,7 @@ export default {
 #header{width:100%; height:50px; color: #fff; background: #e54847; border-bottom: 1px solid #e54847; position: relative;}
 #header h1{ font-size: 18px; text-align: center; line-height: 50px; font-weight: normal; }
 #header i{ position: absolute; left: 5px; top: 50%; margin-top: -13px; font-size: 26px;}
-#detailContainer{background: white; z-index: 100; position: absolute; left: 0; top: 0; width: 100%; min-height: 100%;}
+#detailContainer{background: white; z-index: 100; position: absolute; left: 0; top: 0; width: 100%;}
 .slide_enter{animation: .3s slideMove;}
 @keyframes slideMove{
     0% {transform: translateX(100%);}
@@ -110,7 +110,7 @@ export default {
 }
 #content.contentDetail{ display: block; margin-bottom:0;}
 #content .detail_list{ height:200px; width:100%; position: relative; overflow: hidden;}
-.detail_list .detail_list_bg{ width:100%; height:100%; background: url(/images/movie_1.jpg) 0 40%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
+.detail_list .detail_list_bg{ width:100%; height:100%; background: 0 40%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
 .detail_list .detail_list_filter{ width:100%; height:100%; position: absolute;background-color: #40454d;opacity: .55; position: absolute; left: 0; top: 0; z-index: 1;}
 .detail_list .detail_list_content{ display: flex; width:100%; height:100%; position: absolute; left: 0; top: 0; z-index: 2;}
 .detail_list .detail_list_img{ width:108px; height: 150px; border: solid 1px #f0f2f3; margin:20px;}
@@ -122,7 +122,7 @@ export default {
 #content .detail_intro{ padding: 10px;}
 #content .detail_player{ margin:20px;}
 .detail_player .swiper-slide{ width:70px; margin-right: 20px; text-align: center; font-size: 14px;}
-.detail_player .swiper-slide img{ width:100%; margin-bottom: 5px;}
+.detail_player .swiper-slide img{ width:100%; margin-bottom: 5px; height: 80px;}
 .detail_player .swiper-slide p:nth-of-type(2){ color:#999;}
 .fixed3{ position: fixed; top: 0; left: 0;}
 </style>
